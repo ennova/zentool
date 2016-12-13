5ffcb5895fd9c499bde4a94ff5f74d3e50b86f28
Jared Sharplin <Jared@Ennovas-MacBook-Pro.local>
2016-12-13 16:30:47 +1000

    Added credentials prompt + bug fixing

--- lib/zentool/zendesk_ticket.rb
+++ lib/zentool/zendesk_ticket.rb
@@ -4,12 +4,14 @@ require 'httparty'
 require 'ruby-progressbar'
 require 'csv'
 require 'pry'
+require 'optparse'
 
 class Zendesk
   def initialize
     @root_uri = 'https://envisionapp.zendesk.com/api/v2/'
     @start_time = Time.new('2016-01-01').to_i
     @tickets_uri = @root_uri + "incremental/tickets.json?start_time=#{@start_time}"
+    check_auth
   end
 
   def tickets
@@ -42,47 +44,54 @@ class Zendesk
 
   def basic_auth
     {
-      :basic_auth => {
-        :username => 'adrian.smith@ennova.com.au',
-        :password => 'Envision_Help13'
-      }
+      basic_auth: {
+        username: $zendesk_username,
+        password: $zendesk_password,
+      },
     }
+  end
+
+  def check_auth
+    response = HTTParty.get(@tickets_uri, basic_auth)
+    unless response.code == 200
+      puts "Error #{response.code}: #{response.message}"
+      abort
+    end
   end
 end
 
 # Represents a single ticket, including its data and metrics
 class Ticket
+  attr_accessor :metrics, :info
+
   def initialize(info, metrics)
       @info = info
       @metrics = metrics
   end
-
-  def info
-    @info
-  end
-
-  def metrics
-    @metrics
-  end
 end
 
 # Represents an aggregate of metrics from multiple tickets 
-class Metrics
-  def initialize(tickets)
-    @tickets = tickets
-    @tickets_by_age = Hash.new {|age, id|}
-    @tickets_by_reply = Hash.new {|priority, avg_reply_time|}
-    tickets.each do |ticket|
-  end
+# class Metrics
+#   def initialize(tickets)
+#     @tickets = tickets
+#     @tickets_by_age = Hash.new {|age, id|}
+#     @tickets_by_user_priority = Hash.new {|user_priority, [reply_times]|}
+#     @tickets_by_development_priority = Hash.new {|development_priority, [reply_times]|}
+#     tickets.each do |ticket|
+#     	age = floor(ticket.metrics['age'] / 1440)
+#     	@tickets_by_age.update[:age]
+#     	@tickets_by_user_priority[ticket['user_priority']]
 
-  def tickets_by_age
+#   end
+
+#   def tickets_by_age
     
-  end
+#   end
 
-  def tickets_by_reply
+#   def tickets_by_reply
 
-  end
-end
+#   end
+# end
 
 # Contains methods for generating a suite of graphs based on ticket metrics
 class Graphs
@@ -93,6 +102,55 @@ end
 
 # begin script
 system 'clear'
+
+options = {}
+
+OptionParser.new do |parser|
+  parser.banner = "Usage: zentool [options]"
+
+  parser.on("-h", "--help", "Show this help message") do ||
+    puts parser
+  end
+
+  parser.on("-u", "--username USERNAME", "The username for the Zendesk.") do |v|
+    options[:username] = v
+  end
+
+  parser.on("-p", "--password PASSWORD", "The password for the Zendesk.") do |v|
+    options[:password] = v
+  end
+
+  parser.on("-l", "--link LINK", "The Zendesk URL.") do |v|
+    options[:url] = v
+  end
+end.parse!
+
+def wrap(s, width = 20)
+  s.gsub(/(.{1,#{width}})(\s+|\Z)/, "\\1\n")
+end
+
+if options[:url] == NilClass || !options.key?(:url)
+  print 'Zendesk URL: '
+  options[:url] = gets.chomp
+  puts
+end
+if options[:username] == NilClass || !options.key?(:username)
+  print 'Zendesk username: '
+  options[:username] = gets.chomp
+  puts
+end
+if options[:password] == NilClass || !options.key?(:password)
+  print 'Zendesk password: '
+  options[:password] = STDIN.noecho(&:gets).chomp
+  puts
+end
+
+puts
+
+$zendesk_url = options[:url]
+$zendesk_username = options[:username]
+$zendesk_password = options[:password]
+
 puts 'Envision Zendesk Tickets'
 puts '------------------------'
 
@@ -103,47 +161,47 @@ puts '   Total tickets = ' + tickets_in.count.to_s
 puts
 
 puts '-> Generating tickets summary file: all_tickets.csv'
-progressbar = ProgressBar.create(title: "#{tickets.count} Tickets", starting_at: 1, format: '%a |%b>>%i| %p%% %t', total: tickets_in.count)
+progressbar = ProgressBar.create(title: "#{tickets_in.count} Tickets", starting_at: 1, format: '%a |%b>>%i| %p%% %t', total: tickets_in.count)
 
 CSV.open("all_tickets.csv", "wb") do |csv|
   csv << zendesk.export_columns + zendesk.metric_columns
-CSV.open("tickets_by_age.csv", "wb") do |csv|
-  csv << zendesk.metric_columns
-CSV.open("tickets_by_reply_time.csv", "wb") do |csv|
-  csv << zendesk.metric_columns
+# CSV.open("tickets_by_age.csv", "wb") do |csv|
+#   csv << zendesk.metric_columns
+# CSV.open("tickets_by_reply_time.csv", "wb") do |csv|
+#   csv << zendesk.metric_columns
 end
 
-# Import tickets from Zendesk into instances of Ticket class
+puts 'Importing tickets from Zendesk into instances of Ticket class'
 
-tickets_in.each do |ticket|
-
-    info = []
-    metrics_info = []
-    zendesk.export_colums.each do |column|
+tickets = Array.new
+tickets_in.first(10).each do |ticket|
+    info = Hash.new
+    metrics_info = Hash.new
+    zendesk.export_columns.each do |column|
       case column
       when 'type'
-        info << ticket['custom_fields'][0]['value']
+        info['type'] = ticket['custom_fields'][0]['value']
       when 'user_priority'
-        info << ticket['custom_fields'][1]['value']
+        info['user_priority'] = ticket['custom_fields'][1]['value']
       when 'development_priority'
         value = ticket['custom_fields'][2]['value']
         if value
-          info << "d#{value[-1]}" if value[-1].to_i > 0
+          info['development_priority'] = "d#{value[-1]}" if value[-1].to_i > 0
         else
-          info << value
+          info['development_priority'] = value
         end
       when 'company'
-        info << ticket['custom_fields'][3]['value']
+        info['company'] = ticket['custom_fields'][3]['value']
       when 'project'
-        info << ticket['custom_fields'][4]['value']
+        info['project'] = ticket['custom_fields'][4]['value']
       when 'platform'
-        info << ticket['custom_fields'][5]['value']
+        info['platform'] = ticket['custom_fields'][5]['value']
       when 'function'
-        info << ticket['custom_fields'][6]['value']
+        info['function'] = ticket['custom_fields'][6]['value']
       when 'satisfaction_rating'
-        info << ticket['satisfaction_rating']['score']
+        info['satisfaction_rating'] = ticket['satisfaction_rating']['score']
       else
-        info << ticket[column]
+        info[column] = ticket[column]
       end
     end
 
@@ -153,27 +211,37 @@ tickets_in.each do |ticket|
         if metrics['ticket_metric']
           case column
           when 'solved_at'
-            metrics_info << metrics['ticket_metric'][column]
+            metrics_info[column] = metrics['ticket_metric'][column]
           else
-            metrics_info << metrics['ticket_metric'][column]['business']
+            metrics_info[column] = metrics['ticket_metric'][column]['business']
           end
         end
       end
     rescue
       retry
     end
+	progressbar.increment
     this = Ticket.new(info, metrics_info)
+    tickets << this
+
 end
 
-# Write ticket information to a csv file
+
+puts 'Writing ticket information to all_tickets.csv'
 tickets.each do |ticket|
+	row = []
   CSV.open("all_tickets.csv", "a") do |csv|
-    row = []
+    row << ticket.info
+    row << ticket.metrics
     csv << row
-    progressbar.increment
+
   end
 end
 
+
+# metrics = Metrics.new(tickets)
+
 # Agreggate ticket metrics
+# tickets.each do |ticket|
 
 # Generate graphs from aggregates
