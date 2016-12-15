0558637dd40b55f5da99bc01ea963e729ab88c12
Tom Stephen <tom.stephen@me.com>
2016-12-15 12:18:34 +1000

    More tests for graph class

--- spec/graph_spec.rb
+++ spec/graph_spec.rb
@@ -39,35 +39,34 @@ describe Graph do
     end
     context "given 'Hello this is a test string being used for photosythetic supercalifragilisticexpialadocius'" do
        it "returns 'Hello this is a\ntest string being used for\nphotosythetic\nsupercalifragilisticexpialadocius\n'" do
-         expect(Graph.wrap("Hello this is a\ntest string being used for photosythetic supercalifragilisticexpialadocius")).to eql("Hello this is a test string\nbeing used for\nphotosythetic\nsupercalifragilisticexpialadocius\n")
+         expect(Graph.wrap("Hello this is a test string being used for photosythetic supercalifragilisticexpialadocius")).to eql("Hello this is a\ntest string\nbeing used for\nphotosythetic\nsupercalifragilisticexpialadocius\n")
        end
      end
   end
-end
 
-  # describe '.extract_links' do
-  #   context "given 'hello here is a link https://www.google.com that was the link'" do
-  #     it 'returns [https://www.google.com]' do
-  #       expect(Graph.extract_links('hello here is a link https://www.google.com that was the link')).to eql(['https://www.google.com'])
-  #     end
-  #   end
-  #   context "given 'hello here https://www.reddit.com is a link https://www.google.com that was the link'" do
-  #     it 'returns [https://www.reddit.com, https://www.google.com]' do
-  #       expect(Graph.extract_links('hello here https://www.reddit.com is a link https://www.google.com that was the link')).to eql(['https://www.reddit.com','https://www.google.com'])
-  #     end
-  #   end
-  # end
-  #
-  # describe '.extract_IDs' do
-  #   context "given 'https://www.google.com/1231296234/bla'" do
-  #     it "returns '1231296234'" do
-  #       expect(Graph.extract_IDs('https://www.google.com/1231296234/bla')).to eql(1231296234)
-  #     end
-  #   end
-  #   context "given 'https://www.google.com/1292/bla'" do
-  #     it "returns '1292'" do
-  #       expect(Graph.extract_IDs('https://www.google.com/1292/bla')).to eql(1292)
-  #     end
-  #   end
-  # end
-# end
+  describe '.extract_links' do
+    context "given 'hello here is a link https://www.google.com that was the link'" do
+      it 'returns [https://www.google.com]' do
+        expect(Graph.extract_links('hello here is a link https://www.google.com that was the link')).to eql(['https://www.google.com'])
+      end
+    end
+    context "given 'hello here https://www.reddit.com is a link https://www.google.com that was the link'" do
+      it 'returns [https://www.reddit.com, https://www.google.com]' do
+        expect(Graph.extract_links('hello here https://www.reddit.com is a link https://www.google.com that was the link')).to eql(['https://www.reddit.com','https://www.google.com'])
+      end
+    end
+  end
+
+  describe '.extract_IDs' do
+    context "given 'https://www.google.com/1231296234/bla'" do
+      it "returns '1231296234'" do
+        expect(Graph.extract_IDs('https://www.google.com/1231296234/bla')).to eql(1231296234)
+      end
+    end
+    context "given 'https://www.google.com/1292/bla'" do
+      it "returns '1292'" do
+        expect(Graph.extract_IDs('https://www.google.com/1292/bla')).to eql(1292)
+      end
+    end
+  end
+end
